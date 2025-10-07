f45b2b88f757aabe5bb5617bb9f8a21de9456aa6
Thauã Mengue <thaua.mengue@gmail.com>
2025-10-06 23:19:31 -0300

    Implementando regras de autenticação

---
+++ lib/service/auth_service.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  User? get currentUser => _auth.currentUser;
+
+  Future<UserCredential> registerWithEmail(String email, String password) async {
+    try {
+      final cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      await cred.user?.sendEmailVerification();
+      return cred;
+    } on FirebaseAuthException catch (e) {
+      throw Exception('Erro ao cadastrar: ${e.message}');
+    }
+  }
+
+  Future<UserCredential> signInWithEmail(String email, String password) async {
+    try {
+      final cred = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      if (!cred.user!.emailVerified) {
+        throw Exception('E-mail não verificado');
+      }
+      return cred;
+    } on FirebaseAuthException catch (e) {
+      throw Exception('Erro ao fazer login: ${e.message}');
+    }
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return null;
+
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      return await _auth.signInWithCredential(credential);
+    } catch (e) {
+      throw Exception('Erro ao fazer login com Google: $e');
+    }
+  }
+
+  Future<void> signOut() async {
+    await _googleSignIn.signOut();
+    await _auth.signOut();
+  }
+}
